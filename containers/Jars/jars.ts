09e2b6fb33ef5b09bc0870a93342b1c43931859d
larrythecucumber321 <71284258+larrythecucumber321@users.noreply.github.com>
2021-02-06 23:01:31 -0800

    Change link for yveCRV pool

--- containers/Jars/jars.ts
+++ containers/Jars/jars.ts
@@ -112,7 +112,7 @@ export const DEPOSIT_TOKENS_LINK = {
   SUSHI_ETH_YFI:
     "https://exchange.sushiswapclassic.org/#/add/0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e/ETH",
   SUSHI_ETH_YVECRV:
-    "https://exchange.sushiswapclassic.org/#/add/0xc5bddf9843308380375a611c18b50fb9341f502a/ETH",
+    "https://sushiswap.fi/pair/0x10b47177e92ef9d5c6059055d92ddf6290848991",
   DAI: "https://etherscan.io/token/0x6b175474e89094c44da98b954eedeac495271d0f",
 };
 
