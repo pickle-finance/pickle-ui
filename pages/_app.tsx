66a9cc5518aa73a7425ed342fc165808e1516a21
Manny Reimi <60312391+mannyreimi88@users.noreply.github.com>
2021-03-01 21:58:45 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -77,6 +77,27 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
                   `,
           }}
         />
+        
+        <!-- Download Canny SDK -->
+        <script>!function(w,d,i,s){function l(){if(!d.getElementById(i)){var f=d.getElementsByTagName(s)[0],e=d.createElement(s);e.type="text/javascript",e.async=!0,e.src="https://canny.io/sdk.js",f.parentNode.insertBefore(e,f)}}if("function"!=typeof w.Canny){var c=function(){c.q.push(arguments)};c.q=[],w.Canny=c,"complete"===d.readyState?l():w.attachEvent?w.attachEvent("onload",l):w.addEventListener("load",l,!1)}}(window,document,"canny-jssdk","script");</script>
+
+        <!-- Use the Canny SDK to identify the current user of your website -->
+        <script>
+        Canny('identify', {
+          appID: '603cf06a10aac45a5b355b04',
+          user: {
+            // Replace these values with the current user's data
+            email: user.email,
+            name: user.name,
+            id: user.id,
+
+            // These fields are optional, but recommended:
+            avatarURL: user.avatarURL,
+            created: new Date(user.created).toISOString(),
+          },
+        });
+        </script>
+        
         <meta property="og:title" content="Farm PICKLE tokens" />
         <meta
           property="og:description"
