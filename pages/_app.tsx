8fc965f2f21de5b345fbab14fe03d70313682669
larrythecucumber321 <larrythecucumber321@gmail.com>
2021-03-09 21:58:12 -0800

    refactor SDK scripts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -75,22 +75,9 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
                     gtag('config', 'G-R1CT5KTZCB', {
                       page_path: window.location.pathname,
                     });
-                  `,
-          }}
-        />
-        
-        
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-                    >!function(w,d,i,s){function l(){if(!d.getElementById(i)){var f=d.getElementsByTagName(s)[0],e=d.createElement(s);e.type="text/javascript",e.async=!0,e.src="https://canny.io/sdk.js",f.parentNode.insertBefore(e,f)}}if("function"!=typeof w.Canny){var c=function(){c.q.push(arguments)};c.q=[],w.Canny=c,"complete"===d.readyState?l():w.attachEvent?w.attachEvent("onload",l):w.addEventListener("load",l,!1)}}(window,document,"canny-jssdk","script");
-                    `
-          }}
-        />
 
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
+                    >!function(w,d,i,s){function l(){if(!d.getElementById(i)){var f=d.getElementsByTagName(s)[0],e=d.createElement(s);e.type="text/javascript",e.async=!0,e.src="https://canny.io/sdk.js",f.parentNode.insertBefore(e,f)}}if("function"!=typeof w.Canny){var c=function(){c.q.push(arguments)};c.q=[],w.Canny=c,"complete"===d.readyState?l():w.attachEvent?w.attachEvent("onload",l):w.addEventListener("load",l,!1)}}(window,document,"canny-jssdk","script");
+
                     Canny('identify', {
                       appID: '603cf06a10aac45a5b355b04',
                       user: {
@@ -104,7 +91,7 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
                         created: new Date(user.created).toISOString(),
                       },
                     });
-                    `
+                  `,
           }}
         />
         
