0e5b1a35b69c7e5f7a9e5bf5a4d454b7c874c68b
Abiencode <abiencode@gmail.com>
2021-04-13 05:20:30 -0700

    Replace alchemy with infura

--- next.config.js
+++ next.config.js
@@ -4,8 +4,7 @@ module.exports = {
     ignoreBuildErrors: true,
   },
   env: {
-    Alchemy:
-      "https://eth-mainnet.alchemyapi.io/v2/c8Ssq_zNMjrh1Cg739W_F5_UcAzHFU1B",
+    infura: "https://mainnet.infura.io/v3/5fa581aa3c064db2a59a0da57b6f984f",
     Fortmatic: "pk_live_3754096A894BEFE4",
     Portis: "8f879477-6443-4f75-8e94-b44aee86a9f7",
   },
