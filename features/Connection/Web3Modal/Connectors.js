eb31adb4282d55cafc5e413b7d63cf27ce30f32c
Abiencode <abiencode@gmail.com>
2021-04-20 02:33:47 -0700

    Remove portfis and fortmatic, update infura link

--- features/Connection/Web3Modal/Connectors.js
+++ features/Connection/Web3Modal/Connectors.js
@@ -1,9 +1,6 @@
 import { InjectedConnector } from "@web3-react/injected-connector";
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 import { WalletLinkConnector } from "@web3-react/walletlink-connector";
-import { FortmaticConnector } from "@web3-react/fortmatic-connector";
-import { PortisConnector } from "@web3-react/portis-connector";
-
 const POLLING_INTERVAL = 12000;
 const RPC_URLS = {
   1: process.env.infura,
@@ -24,14 +21,4 @@ export const walletlink = new WalletLinkConnector({
   url: RPC_URLS[1],
   appName: "Pickle Finance",
   appLogoUrl: "pickle.png",
-});
-
-export const fortmatic = new FortmaticConnector({
-  apiKey: process.env.Fortmatic,
-  chainId: 1,
-});
-
-export const portis = new PortisConnector({
-  dAppId: process.env.Portis,
-  networks: [1],
 });
--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     ignoreBuildErrors: true,
   },
   env: {
-    infura: "https://mainnet.infura.io/v3/5fa581aa3c064db2a59a0da57b6f984f",
+    infura: "https://mainnet.infura.io/v3/f7fd6784e0604b7ea588ba7b0f9843fa",
     Fortmatic: "pk_live_3754096A894BEFE4",
     Portis: "8f879477-6443-4f75-8e94-b44aee86a9f7",
   },
