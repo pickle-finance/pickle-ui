5fd29cb4fd09f3de067d9b39324771b10d8bbf9c
larrythecucumber321 <larrythecucumber321@gmail.com>
2021-04-21 23:50:59 -0700

    correct pending pickles

--- features/Balances/usePendingPickles.ts
+++ features/Balances/usePendingPickles.ts
@@ -2,30 +2,20 @@ import { useEffect, useState } from "react";
 import { BigNumber, ethers } from "ethers";
 import { Connection } from "../../containers/Connection";
 import { Contracts } from "../../containers/Contracts";
+import { UserGauges } from "../../containers/UserGauges";
 
 export const usePendingPickles = (): { pendingPickles: number | null } => {
   const { address, blockNum } = Connection.useContainer();
-  const { masterchef } = Contracts.useContainer();
+  const { gaugeData } = UserGauges.useContainer();
   const [pendingPickles, setPendingPickles] = useState<number | null>(null);
 
   const getData = async () => {
-    if (address && masterchef) {
-      const poolLengthBN = await masterchef.poolLength();
-      const poolLength = poolLengthBN.toNumber();
-
-      // create array of promises, one for each pool
-      const promises: Array<Promise<BigNumber>> = Array(
-        parseInt(poolLength.toString()),
-      )
-        .fill(0)
-        .map((_, poolIndex) => masterchef.pendingPickle(poolIndex, address));
-
-      // wait for all promises to resolve
-      const pendingPickles = await Promise.all(promises);
-
+    if (address && gaugeData) {
       // add up all the pending pickles from each pool
-      const totalPendingPickles = pendingPickles.reduce(
-        (a, b) => a + Number(ethers.utils.formatUnits(b)),
+      const totalPendingPickles = gaugeData.reduce(
+        (a, b) => a + parseFloat(
+          ethers.utils.formatEther(b.harvestable || 0),
+        ),
         0,
       );
 
@@ -35,7 +25,7 @@ export const usePendingPickles = (): { pendingPickles: number | null } => {
 
   useEffect(() => {
     getData();
-  }, [address, blockNum, masterchef]);
+  }, [address, blockNum, gaugeData]);
 
   return { pendingPickles };
 };
