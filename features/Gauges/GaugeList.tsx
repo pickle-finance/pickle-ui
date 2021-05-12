c372690520ab52c6d9352a7c8572fd166ef998c6
larrythecucumber321 <larrythecucumber321@gmail.com>
2021-05-12 00:59:20 -0700

    add toggle for user jars/farms

--- features/Gauges/GaugeList.tsx
+++ features/Gauges/GaugeList.tsx
@@ -1,6 +1,9 @@
 import { FC, useEffect, useState } from "react";
+import { formatEther } from "ethers/lib/utils";
 import styled from "styled-components";
 import { Spacer, Grid, Checkbox, Button, Input } from "@geist-ui/react";
+import { withStyles } from "@material-ui/core/styles";
+import Switch from "@material-ui/core/Switch";
 import { PercentageInput } from "../../components/PercentageInput";
 import { GaugeCollapsible } from "./GaugeCollapsible";
 import { UserGaugeData, UserGauges } from "../../containers/UserGauges";
@@ -9,6 +12,7 @@ import { TransactionStatus, useGaugeProxy } from "../../hooks/useGaugeProxy";
 import { VoteCollapsible } from "./VoteCollapsible";
 import { GaugeChartCollapsible } from "./GaugeChartCollapsible";
 import { PICKLE_JARS } from "../../containers/Jars/jars";
+import { backgroundColor, pickleGreen } from "../../util/constants";
 
 const Container = styled.div`
   padding-top: 1.5rem;
@@ -18,18 +22,32 @@ interface Weights {
   [key: string]: number;
 }
 
+const GreenSwitch = withStyles({
+  switchBase: {
+    color: backgroundColor,
+    "&$checked": {
+      color: pickleGreen,
+    },
+    "&$checked + $track": {
+      backgroundColor: pickleGreen,
+    },
+  },
+  checked: {},
+  track: {},
+})(Switch);
+
 export const GaugeList: FC = () => {
   const { signer } = Connection.useContainer();
   const { gaugeData } = UserGauges.useContainer();
   const [showInactive, setShowInactive] = useState<boolean>(false);
   const [voteWeights, setVoteWeights] = useState<Weights>({});
   const { status: voteTxStatus, vote } = useGaugeProxy();
+  const [showUserGauges, setShowUserGauges] = useState<boolean>(false);
 
   let totalGaugeWeight = 0;
   for (let i = 0; i < gaugeData?.length; i++) {
     totalGaugeWeight += voteWeights[gaugeData[i].address] || 0;
   }
-
 
   if (!signer) {
     return <h2>Please connect wallet to continue</h2>;
@@ -43,22 +61,34 @@ export const GaugeList: FC = () => {
     depositToken === PICKLE_JARS.pUNIBACDAI ||
     depositToken === PICKLE_JARS.pUNIBASDAI;
 
-  const activeGauges = gaugeData.filter((x) => !isDisabledFarm(x.depositToken.address));
+  const activeGauges = gaugeData.filter(
+    (x) => !isDisabledFarm(x.depositToken.address),
+  );
   const inactiveGauges = gaugeData.filter((x) => false);
+  const userGauges = gaugeData.filter((gauge) =>
+    parseFloat(formatEther(gauge.staked)),
+  );
 
-  const indexofYvboost = activeGauges.findIndex(x=>x.address.toLowerCase() === PICKLE_JARS.pyvBOOSTETH.toLowerCase())-1
+  const indexofYvboost =
+    activeGauges.findIndex(
+      (x) => x.address.toLowerCase() === PICKLE_JARS.pyvBOOSTETH.toLowerCase(),
+    ) - 1;
 
-  const indexofAlcx = activeGauges.findIndex(x=>x.address.toLowerCase() === PICKLE_JARS.pSUSHIETHALCX.toLowerCase())-1
+  const indexofAlcx =
+    activeGauges.findIndex(
+      (x) =>
+        x.address.toLowerCase() === PICKLE_JARS.pSUSHIETHALCX.toLowerCase(),
+    ) - 1;
 
   const moveInArray = (arr: UserGaugeData[], from: number, to: number) => {
     var item = arr.splice(from, 1);
-  
+
     if (!item.length) return;
     arr.splice(to, 0, item[0]);
   };
 
-  moveInArray(activeGauges, indexofAlcx, 2)
-  moveInArray(activeGauges, indexofYvboost, 2)
+  moveInArray(activeGauges, indexofAlcx, 2);
+  moveInArray(activeGauges, indexofYvboost, 2);
 
   const renderGauge = (gauge: UserGaugeData) => (
     <Grid xs={24} key={gauge.address}>
@@ -86,13 +116,23 @@ export const GaugeList: FC = () => {
             onChange={(e) => setShowInactive(e.target.checked)}
           >
             Show Inactive Farms
-          </Checkbox>
+          </Checkbox>{" "}
+          <GreenSwitch
+            style={{ top: "-2px" }}
+            checked={showUserGauges}
+            onChange={() => setShowUserGauges(!showUserGauges)}
+          />
+          Show Your Farms
         </Grid>
       </Grid.Container>
       <h2>Current Weights</h2>
       <GaugeChartCollapsible gauges={activeGauges} />
       <h2>Vote</h2>
-      <VoteCollapsible gauges={activeGauges.filter(x => x.depositToken.address != PICKLE_JARS.pSUSHIETHYVECRV)} />
+      <VoteCollapsible
+        gauges={activeGauges.filter(
+          (x) => x.depositToken.address != PICKLE_JARS.pSUSHIETHYVECRV,
+        )}
+      />
       <div
         css={{
           justifyContent: "space-between",
@@ -102,7 +142,7 @@ export const GaugeList: FC = () => {
       >
         <h2>Active Farms</h2>
       </div>
-      <Grid.Container gap={1}>{activeGauges.map(renderGauge)}</Grid.Container>
+      <Grid.Container gap={1}>{(showUserGauges ? userGauges : activeGauges).map(renderGauge)}</Grid.Container>
       <Spacer y={1} />
       <Grid.Container gap={1}>
         {showInactive && <h2>Inactive Farms</h2>}
