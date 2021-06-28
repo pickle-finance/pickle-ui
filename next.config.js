7d7d88b88fef68d6bc1f7aa56b3797b8a41ab468
larrythecucumber321 <larrythecucumber321@gmail.com>
2021-06-28 11:28:49 -0700

    update infura api url

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
     infura: "https://mainnet.infura.io/v3/f7fd6784e0604b7ea588ba7b0f9843fa",
     ethRPC: "https://mainnet.infura.io/v3/f7fd6784e0604b7ea588ba7b0f9843fa",
     polygonRPC:
-      "https://polygon-mainnet.infura.io/v3/45ba160b9eda48adb5188c83a5e5ca95",
+      "https://polygon-mainnet.infura.io/v3/bd53a4cab2b04a12a68161921144de55",
     Fortmatic: "pk_live_3754096A894BEFE4",
     Portis: "8f879477-6443-4f75-8e94-b44aee86a9f7",
   },
