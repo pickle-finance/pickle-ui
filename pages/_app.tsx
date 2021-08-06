bb75a9b4b4538b25651df370fbaa20e51338dd61
Jan Klimo <jan.klimo@gmail.com>
2021-08-06 17:13:11 +0700

    Remove Canny script
    Closes #47

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -102,28 +102,6 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
           src="https://www.googletagmanager.com/gtag/js?id=G-R1CT5KTZCB"
         ></script>
 
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-                    !function(w,d,i,s){function l(){if(!d.getElementById(i)){var f=d.getElementsByTagName(s)[0],e=d.createElement(s);e.type="text/javascript",e.async=!0,e.src="https://canny.io/sdk.js",f.parentNode.insertBefore(e,f)}}if("function"!=typeof w.Canny){var c=function(){c.q.push(arguments)};c.q=[],w.Canny=c,"complete"===d.readyState?l():w.attachEvent?w.attachEvent("onload",l):w.addEventListener("load",l,!1)}}(window,document,"canny-jssdk","script");
-
-                    Canny('identify', {
-                      appID: '603cf06a10aac45a5b355b04',
-                      user: {
-                        // Replace these values with the current user's data
-                        email: user.email,
-                        name: user.name,
-                        id: user.id,
-
-                        // These fields are optional, but recommended:
-                        avatarURL: user.avatarURL,
-                        created: new Date(user.created).toISOString(),
-                      },
-                    });
-                  `,
-          }}
-        />
-
         <meta property="og:title" content="Optimize your yield" />
         <meta
           property="og:description"
