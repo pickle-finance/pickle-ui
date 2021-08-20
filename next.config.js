62427cc595d53de6024273478064a599ceb11faf
Jan Klimo <jan.klimo@gmail.com>
2021-08-20 18:02:26 +0700

    Use Sentry

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+const { withSentryConfig } = require("@sentry/nextjs");
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withBundleAnalyzer({
+const moduleExports = {
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -15,4 +21,20 @@ module.exports = withBundleAnalyzer({
     Portis: "8f879477-6443-4f75-8e94-b44aee86a9f7",
     apiHost: "https://api.pickle.finance/prod",
   },
-});
+};
+
+const SentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+module.exports = withBundleAnalyzer(
+  withSentryConfig(moduleExports, SentryWebpackPluginOptions),
+);
