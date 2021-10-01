41bc3e689d2d211cc947d75940feb7d8c8b0cc43
Jan Klimo <jan.klimo@gmail.com>
2021-10-01 19:43:58 +0700

    i18n: BalFarm

--- features/PickleFarms/BalFarm.tsx
+++ features/PickleFarms/BalFarm.tsx
@@ -1,13 +1,12 @@
-import { ethers } from "ethers";
 import React, { useState, FC, useEffect } from "react";
-import { Grid, Card, Spacer } from "@geist-ui/react";
+import { Grid, Card } from "@geist-ui/react";
+import { formatEther } from "@ethersproject/units";
+import { Trans, useTranslation } from "next-i18next";
+
 import { TradeButton } from "features/Prices/Prices";
-import erc20 from "@studydefi/money-legos/erc20";
 import { Connection } from "containers/Connection";
 import { Prices } from "containers/Prices";
-import { config } from "containers/config";
 import { Contracts } from "containers/Contracts";
-import { formatEther } from "@ethersproject/units";
 
 const BALANCER_VAULT = "0xba12222222228d8ba445958a75a0704d566bf2c8";
 
@@ -16,6 +15,7 @@ export const BalFarm: FC = () => {
   const { pickle } = Contracts.useContainer();
   const { prices } = Prices.useContainer();
   const [apy, setApy] = useState("0");
+  const { t } = useTranslation("common");
 
   const getBalancerStats = async () => {
     if (provider && prices && pickle) {
@@ -32,66 +32,62 @@ export const BalFarm: FC = () => {
   }, [blockNum]);
 
   return (
-    <>
-      <Grid.Container gap={2}>
-        <Grid xs={24} sm={14} md={14}>
-          <Card>
-            <Grid.Container>
-              <Grid xs={14} md={16}>
-                <div style={{ fontSize: "large" }}>
-                  {" "}
-                  Farm PICKLE on Balancer
-                  <img
-                    style={{ width: "50px", verticalAlign: "middle" }}
-                    src="balancer.png"
-                  />
-                </div>
-                at{" "}
+    <Grid.Container gap={2}>
+      <Grid xs={24} sm={14} md={14}>
+        <Card>
+          <Grid.Container>
+            <Grid xs={14} md={16}>
+              <div style={{ fontSize: "large" }}>
+                {t("farms.arbitrum.balancer")}
+                <img
+                  style={{ width: "50px", verticalAlign: "middle" }}
+                  src="balancer.png"
+                />
+              </div>
+              <Trans i18nKey="farms.arbitrum.balancerApr">
+                at
                 <b
                   style={{
                     color: "#53ffe2",
                   }}
                 >
-                  {apy}%
-                </b>{" "}
+                  {{ percent: apy }}
+                </b>
                 APR
-              </Grid>
-              <Grid xs={10} md={8}>
-                <TradeButton
-                  href={
-                    "https://arbitrum.balancer.fi/#/pool/0xc2f082d33b5b8ef3a7e3de30da54efd3114512ac000200000000000000000017"
-                  }
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  style={{ marginTop: "7px" }}
-                >
-                  Add liquidity
-                </TradeButton>
-              </Grid>
-            </Grid.Container>
-          </Card>
-        </Grid>
-        <Grid xs={24} sm={10} md={10}>
-          <Card style={{ height: "100%" }}>
-            {" "}
-            <Grid.Container>
-              <Grid xs={12}>
-                <div>Bridge your Ethereum assets to Arbitrum</div>
-              </Grid>
-              <Grid xs={12}>
-                <TradeButton
-                  href={"https://bridge.arbitrum.io/"}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  style={{ marginTop: "7px" }}
-                >
-                  Bridge
-                </TradeButton>
-              </Grid>
-            </Grid.Container>
-          </Card>
-        </Grid>
-      </Grid.Container>
-    </>
+              </Trans>
+            </Grid>
+            <Grid xs={10} md={8}>
+              <TradeButton
+                href={
+                  "https://arbitrum.balancer.fi/#/pool/0xc2f082d33b5b8ef3a7e3de30da54efd3114512ac000200000000000000000017"
+                }
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ marginTop: "7px" }}
+              >
+                {t("farms.addLiquidity")}
+              </TradeButton>
+            </Grid>
+          </Grid.Container>
+        </Card>
+      </Grid>
+      <Grid xs={24} sm={10} md={10}>
+        <Card style={{ height: "100%" }}>
+          <Grid.Container>
+            <Grid xs={12}>{t("farms.arbitrum.bridgeAssets")}</Grid>
+            <Grid xs={12}>
+              <TradeButton
+                href={"https://bridge.arbitrum.io/"}
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ marginTop: "7px" }}
+              >
+                {t("farms.arbitrum.bridge")}
+              </TradeButton>
+            </Grid>
+          </Grid.Container>
+        </Card>
+      </Grid>
+    </Grid.Container>
   );
 };
