8a5c5205af59c8326822e15e22ed3e0f34747eb3
Jan Klimo <jan.klimo@gmail.com>
2021-10-03 10:45:48 +0700

    i18n: Farms pages

--- pages/farms.tsx
+++ pages/farms.tsx
@@ -1,5 +1,7 @@
 import { FC } from "react";
 import { Page } from "@geist-ui/react";
+import { useTranslation } from "next-i18next";
+
 import { Footer } from "../features/Footer/Footer";
 import { GaugeList } from "../features/Gauges/GaugeList";
 import { MiniFarmList } from "../features/MiniFarms/MiniFarmList";
@@ -8,21 +10,18 @@ import { NETWORK_NAMES } from "containers/config";
 
 const Gauges: FC = () => {
   const { chainName } = Connection.useContainer();
+  const { t } = useTranslation("common");
 
   return (
-    <>
-      <Page>
-        <Page.Content>
-          <h1 style={{ fontSize: `2rem`, fontFamily: `Source Code Pro` }}>
-            Farms
-          </h1>
-          {chainName === NETWORK_NAMES.ETH ? (
-            <GaugeList />
-          ) : <MiniFarmList />}
-        </Page.Content>
-        <Footer />
-      </Page>
-    </>
+    <Page>
+      <Page.Content>
+        <h1 style={{ fontSize: `2rem`, fontFamily: `Source Code Pro` }}>
+          {t("info.farms")}
+        </h1>
+        {chainName === NETWORK_NAMES.ETH ? <GaugeList /> : <MiniFarmList />}
+      </Page.Content>
+      <Footer />
+    </Page>
   );
 };
 
--- pages/old-farms.tsx
+++ pages/old-farms.tsx
@@ -1,45 +1,41 @@
 import { FC } from "react";
-import { Page, Note, Grid, Spacer } from "@geist-ui/react";
+import { Page, Note, Spacer } from "@geist-ui/react";
 import { ArrowLeft } from "@geist-ui/react-icons";
+import { Trans, useTranslation } from "next-i18next";
 
 import Link from "next/link";
 import { Footer } from "../features/Footer/Footer";
 import { FarmList } from "../features/Farms/FarmList";
 
 const Farms: FC = () => {
+  const { t } = useTranslation("common");
+
   return (
     <>
       <Page>
         <Page.Content>
-          {/* <strong>
-            <Link href="/farms" style={{ color: "var(--link-color)" }}>
-              <span>
-                <ArrowLeft size={13} />
-                <Spacer x={0.3} inline /> to new Farms
-              </span>
-            </Link>
-          </strong> */}
           <Link href="/farms" passHref>
             <a style={{ textDecoration: "none" }}>
-              {" "}
               <strong>
-                <ArrowLeft size={13} /> to new Farms{" "}
+                <ArrowLeft size={13} /> {t("oldFarms.back")}
               </strong>
             </a>
           </Link>
           <Spacer />
           <Note type="warning" style={{ textAlign: "center" }}>
-            These Farms stopped earning $PICKLEs on Thu, Apr 22, 2021 (12 AM
-            GMT)
-            <br />
-            To earn $PICKLEs, migrate any tokens here to the new Farms
-            <br />
-            Expand on any Farm you have tokens, and click{" "}
-            <strong>Migrate</strong>
+            <Trans i18nKey="oldFarms.disclaimer">
+              These Farms stopped earning $PICKLEs on Thu, Apr 22, 2021 (12 AM
+              GMT)
+              <br />
+              To earn $PICKLEs, migrate any tokens here to the new Farms
+              <br />
+              Expand on any Farm you have tokens, and click
+              <strong>Migrate</strong>
+            </Trans>
           </Note>
 
           <h1 style={{ fontSize: `2rem`, fontFamily: `Source Code Pro` }}>
-            Farms
+            {t("info.farms")}
           </h1>
           <FarmList />
         </Page.Content>
