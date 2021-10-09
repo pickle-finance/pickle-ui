a3de3c3762f793c9008070deba43f856acef39a5
Manny Reimi <mannyreimi@gmail.com>
2021-10-09 18:04:25 +0800

    complete rebase

--- features/MiniFarms/JarMiniFarmCollapsible.tsx
+++ features/MiniFarms/JarMiniFarmCollapsible.tsx
@@ -93,12 +93,6 @@ export const FARM_LP_TO_ICON: {
   "0x1cCDB8152Bb12aa34e5E7F6C9c7870cd6C45E37F": (
     <LpIcon swapIconSrc={"/quickswap.png"} tokenIconSrc={"/dino.jpeg"} />
   ),
-<<<<<<< HEAD
-  "0xD170F0a8629a6F7A1E330D5fC455d96E54c09675": (
-    <LpIcon swapIconSrc={"/sushiswap.png"} tokenIconSrc={"/work.png"} />
-  ),
-=======
->>>>>>> ce9ebe5 (Minor rearrangement)
   "0xC3f393FB40F8Cc499C1fe7FA5781495dc6FAc9E9": (
     <LpIcon swapIconSrc={"/cherryswap.png"} tokenIconSrc={"/okex.png"} />
   ),
