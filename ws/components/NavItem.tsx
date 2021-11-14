37be4e60bb891011f3713f781cf42ee23bca73b4
Jan Klimo <jan.klimo@gmail.com>
2021-11-14 16:53:10 +0700

    Add external token links to navbar

--- ws/components/NavItem.tsx
+++ ws/components/NavItem.tsx
@@ -1,17 +1,27 @@
 import { FC, HTMLAttributes, ComponentProps } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { ExternalLinkIcon } from "@heroicons/react/solid";
+
 import { classNames } from "ws/utils";
 
 interface Props extends HTMLAttributes<HTMLElement> {
   href: string;
-  Icon: (props: ComponentProps<"svg">) => JSX.Element;
+  Icon?: (props: ComponentProps<"svg">) => JSX.Element;
+  isExternal?: boolean;
 }
 
-const NavItem: FC<Props> = ({ href, children, className, Icon }) => {
+const NavItem: FC<Props> = ({
+  href,
+  children,
+  className,
+  Icon,
+  isExternal,
+}) => {
   const extraClasses = className ? className : "";
   const router = useRouter();
-  const current = router.pathname.endsWith(href);
+  const isCurrent = isExternal ? false : router.pathname.endsWith(href);
+  const linkProps = isExternal ? { target: "_blank", rel: "noopener" } : {};
 
   return (
     <div className="flex items-center">
@@ -19,20 +29,29 @@ const NavItem: FC<Props> = ({ href, children, className, Icon }) => {
         <a
           href={href}
           className={classNames(
-            current ? "bg-black-light text-green-light" : "",
+            isCurrent ? "bg-black-light text-green-light" : "",
             `group flex flex-grow items-center px-4 py-2 text-white text-sm rounded-lg font-bold hover:text-green-light transition duration-300 ease-in-out ${extraClasses}`,
           )}
+          {...linkProps}
         >
-          <Icon
-            className={classNames(
-              current
-                ? "text-green-light"
-                : "text-white group-hover:text-green-light",
-              "mr-2 flex-shrink-0 h-6 w-6",
-            )}
-            aria-hidden="true"
-          />
+          {Icon && (
+            <Icon
+              className={classNames(
+                isCurrent
+                  ? "text-green-light"
+                  : "text-white group-hover:text-green-light",
+                "mr-2 flex-shrink-0 h-5 w-5",
+              )}
+              aria-hidden="true"
+            />
+          )}
           {children}
+          {isExternal && (
+            <ExternalLinkIcon
+              className="text-white group-hover:text-green-light ml-2 flex-shrink-0 h-4 w-4"
+              aria-hidden="true"
+            />
+          )}
         </a>
       </Link>
     </div>
--- ws/components/Navbar.tsx
+++ ws/components/Navbar.tsx
@@ -1,36 +1,58 @@
 import { FC } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { useTranslation } from "next-i18next";
 
 import pickleLogo from "public/pickle-logo.png";
 import NavItems from "./NavItems";
+import NavItem from "./NavItem";
 
-const Navbar: FC = () => (
-  <div className="hidden sm:flex sm:flex-col sm:w-64 sm:fixed sm:inset-y-0">
-    <div className="flex-1 flex flex-col min-h-0 bg-black py-8 border-r border-gray-dark">
-      <div className="flex items-center justify-center h-16 flex-shrink-0 px-4 mb-12">
-        <div className="w-32">
-          <Link href="/ws">
-            <a aria-label="Pickle Finance home">
-              <Image
-                src={pickleLogo}
-                width={500}
-                height={191}
-                priority
-                layout="responsive"
-                alt="Pickle Finance home"
-                title="Pickle Finance home"
-                placeholder="blur"
-              />
-            </a>
-          </Link>
+const Navbar: FC = () => {
+  const { t } = useTranslation("common");
+
+  return (
+    <div className="hidden sm:flex sm:flex-col sm:w-64 sm:fixed sm:inset-y-0">
+      <div className="flex-1 flex flex-col min-h-0 bg-black py-8 border-r border-gray-dark">
+        <div className="flex items-center justify-center h-16 flex-shrink-0 px-4 mb-12">
+          <div className="w-32">
+            <Link href="/ws">
+              <a aria-label="Pickle Finance home">
+                <Image
+                  src={pickleLogo}
+                  width={500}
+                  height={191}
+                  priority
+                  layout="responsive"
+                  alt="Pickle Finance home"
+                  title="Pickle Finance home"
+                  placeholder="blur"
+                />
+              </a>
+            </Link>
+          </div>
+        </div>
+        <div className="flex flex-col flex-grow justify-between px-10">
+          <div className="flex flex-col overflow-y-auto">
+            <NavItems />
+          </div>
+          <div className="flex flex-col overflow-y-auto">
+            <NavItem
+              isExternal
+              href="https://etherscan.io/address/0x429881672B9AE42b8EbA0E26cD9C73711b891Ca5"
+            >
+              {t("ws.nav.tokenAddress")}
+            </NavItem>
+            <NavItem
+              isExternal
+              href="https://etherscan.io/address/0xbBCf169eE191A1Ba7371F30A1C344bFC498b29Cf"
+            >
+              {t("ws.nav.dillAddress")}
+            </NavItem>
+          </div>
         </div>
       </div>
-      <div className="flex-1 flex flex-col overflow-y-auto">
-        <NavItems />
-      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Navbar;
