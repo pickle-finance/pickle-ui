1f2baaa972903af42fb073bbd005dd00a4d160d4
Jan Klimo <jan.klimo@gmail.com>
2021-11-14 19:45:16 +0700

    Add SWR

--- ws/utils/index.ts
+++ ws/utils/index.ts
@@ -1,2 +1,20 @@
+/**
+ * Tailwind
+ */
 export const classNames = (...classes: string[]) =>
   classes.filter(Boolean).join(" ");
+
+/**
+ * SWR
+ */
+export const fetcher = (...args: [string]) =>
+  fetch(...args).then((res) => res.json());
+
+/**
+ * Responses
+ */
+export interface PicklePriceResponse {
+  "pickle-finance": {
+    usd: number;
+  };
+}
