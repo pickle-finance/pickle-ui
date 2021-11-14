04e6934933300aec0b3903d4f3089f2e07e8bd2b
Jan Klimo <jan.klimo@gmail.com>
2021-11-14 19:45:16 +0700

    Add SWR

--- ws/components/PicklePriceIndicator.tsx
+++ ws/components/PicklePriceIndicator.tsx
@@ -1,25 +1,45 @@
 import { FC } from "react";
 import Image from "next/image";
+import Skeleton from "@material-ui/lab/Skeleton";
+import useSWR from "swr";
 
 import logo from "../../public/pickle.png";
+import { fetcher, PicklePriceResponse } from "../utils";
 
-const PicklePriceIndicator: FC = () => {
-  return (
-    <div className="flex items-center px-4 py-2 mb-2 text-white text-sm font-bold">
-      <div className="w-10 p-2 bg-black-light rounded-3xl mr-2">
-        <Image
-          src={logo}
-          width={80}
-          height={80}
-          layout="responsive"
-          alt="Pickle Finance"
-          title="Pickle Finance"
-          placeholder="blur"
-        />
-      </div>
-      $19.74
-    </div>
-  );
+const PicklePriceIndicatorBody: FC = () => {
+  const endpoint =
+    "https://api.coingecko.com/api/v3/simple/price?ids=pickle-finance&vs_currencies=usd";
+  const { data } = useSWR<PicklePriceResponse>(endpoint, fetcher);
+
+  if (!data)
+    return (
+      <Skeleton
+        width="45px"
+        height="26px"
+        style={{
+          background: "#0f1f22",
+        }}
+      />
+    );
+
+  return <span>${data["pickle-finance"].usd}</span>;
 };
+
+const PicklePriceIndicator: FC = () => (
+  <div className="flex items-center px-4 py-2 mb-2 text-white text-sm font-bold">
+    <div className="w-10 p-2 bg-black-light rounded-3xl mr-2">
+      <Image
+        src={logo}
+        width={80}
+        height={80}
+        layout="responsive"
+        alt="Pickle Finance"
+        title="Pickle Finance"
+        placeholder="blur"
+      />
+    </div>
+    <PicklePriceIndicatorBody />
+  </div>
+);
 
 export default PicklePriceIndicator;
