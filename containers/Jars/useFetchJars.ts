5b451d0ecba13249d558b08703643eb5e07f1033
rawbdor <rawblem@gmail.com>
2021-11-22 17:46:14 -0500

    Work in progress - step 1, remove deposit token link and deposit token name
    Signed-off-by: rawbdor <rawblem@gmail.com>

--- containers/Jars/useFetchJars.ts
+++ containers/Jars/useFetchJars.ts
@@ -17,7 +17,10 @@ import {
 } from "./jars";
 import { Contract } from "@ethersproject/contracts";
 import { networkInterfaces } from "os";
-import { NETWORK_NAMES } from "containers/config";
+import { NETWORK_NAMES, NETWORK_NAMES_PFCORE_MAP } from "containers/config";
+import { ChainNetwork } from "picklefinance-core";
+import { AssetEnablement, JarDefinition } from "picklefinance-core/lib/model/PickleModelJson";
+import { PickleCore } from "./usePickleCore";
 
 export type Jar = {
   depositToken: Erc20Contract;
@@ -39,10 +42,79 @@ export const useFetchJars = (): { jars: Array<Jar> | null } => {
     chainName,
   } = Connection.useContainer();
   const { controller, strategy, controllerMai } = Contracts.useContainer();
+  const { pickleCore } = PickleCore.useContainer();
+
 
   const [jars, setJars] = useState<Array<Jar> | null>(null);
 
   const getJars = async () => {
+    // I want to use r2, but it breaks the site. Find out why
+    const r1 : Jar[] = await getJarsOldImpl();
+    const r2 : Jar[] = await getJarsPfcoreImpl();
+
+    const stringOnly = (x:Jar) => {
+      return {
+        depositToken: x.depositToken.address,
+        depositTokenName: x.depositTokenName,
+        depositTokenLink: x.depositTokenLink,
+        jarName: x.jarName,
+        contract: x.contract.address,
+      }
+    }
+    const r1a = r1.map((x)=>stringOnly((x)));
+    const r2a = r2.map((x)=>stringOnly((x)));
+    
+    console.log("\n\n\n" + JSON.stringify(r1.map((x)=>stringOnly((x)))));
+    console.log("\n\n\n" + JSON.stringify(r2.map((x)=>stringOnly((x)))));
+    
+    console.log("Lengths:  original: " + r1a.length + ", new: " + r2a.length);
+    for( let i = 0; i < r1a.length; i++ ) {
+      const matchingR2 = r2a.find((x)=>x.depositToken.toLowerCase() === r1a[i].depositToken.toLowerCase());
+      console.log("Index " + i);
+      console.log("Original: " + JSON.stringify(r1a[i]));
+      console.log("New: " + JSON.stringify(matchingR2 || "nothinghere"));
+      
+    }
+    setJars(r1);
+  }
+
+
+  const getJarsPfcoreImpl = async () : Promise<Jar[]> => {
+    if (controller && strategy && multicallProvider && chainName) {
+      const pfcoreChainName : ChainNetwork = NETWORK_NAMES_PFCORE_MAP[chainName];
+      const allJars : JarDefinition[] | undefined = pickleCore?.assets.jars;
+      if( !allJars ) {
+        // Time to return, dead
+        console.log("This shit is dead, cant even find jars");
+        return [];
+      }
+
+      const chainJars : JarDefinition[] = allJars.filter((x)=>x.chain === pfcoreChainName && 
+          x.enablement !== AssetEnablement.PERMANENTLY_DISABLED);
+      console.log("Jars for current chain: " + chainJars ? chainJars.length : 0);
+      const possibleJars : (Jar|undefined)[] = chainJars.map((x)=>{
+        const z : Jar = {
+          depositToken: Erc20Factory.connect(x.depositToken.addr, provider),
+          depositTokenName: x.depositToken.name,
+          jarName: x.id,
+          depositTokenLink: x.depositToken.link,
+          contract: JarFactory.connect(x.contract, provider),
+        };
+        console.log("Created jar struct for: " + x.details.apiKey);
+
+        return z;
+      });
+      const jarsOnly : Jar[] = [];
+      for( let i = 0; i < possibleJars.length; i++ ) {
+        if( possibleJars[i] !== undefined ) 
+          jarsOnly.push(possibleJars[i] as Jar);
+      }
+      return jarsOnly;
+    }
+    return [];
+  };
+
+  const getJarsOldImpl = async () : Promise<Jar[]> => {
     if (controller && strategy && multicallProvider && chainName) {
       const multicallController = new MulticallContract(
         controller.address,
@@ -118,8 +190,9 @@ export const useFetchJars = (): { jars: Array<Jar> | null } => {
           },
         ),
       );
-      setJars(newJars);
+      return newJars;
     }
+    return [];
   };
 
   useEffect(() => {
