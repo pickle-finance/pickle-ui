9ff4fff20a86603a94be15945f6f16710ebfad03
Yogi <yoginth@hey.com>
2021-12-20 18:44:43 +0530

    chore: use Connection.useContainer()

--- hooks/useENS.ts
+++ hooks/useENS.ts
@@ -1,15 +1,16 @@
+import { Connection } from "containers/Connection";
 import { ethers } from "ethers";
 import { useEffect, useState } from "react";
 
 const useENS = (address: string) => {
-  const provider = new ethers.providers.JsonRpcProvider(process.env.infura);
+  const { provider } = Connection.useContainer();
   const [ensName, setENSName] = useState<string | null>(null);
 
   useEffect(() => {
     const resolveENS = async () => {
       if (ethers.utils.isAddress(address)) {
         let ensName = await provider.lookupAddress(address);
-        setENSName(ensName);
+        if (ensName) setENSName(ensName);
       }
     };
     resolveENS();
