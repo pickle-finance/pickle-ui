561be9f8e2942e8a66ce4685e892ecef9b9c06cf
Jan Klimo <jan.klimo@gmail.com>
2021-12-30 17:21:54 +0700

    List supporting networks

--- v2/features/connection/ConnectionStatus.tsx
+++ v2/features/connection/ConnectionStatus.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import Image from "next/image";
 import { Trans, useTranslation } from "next-i18next";
 import { useWeb3React, UnsupportedChainIdError } from "@web3-react/core";
 import type { Web3Provider } from "@ethersproject/providers";
@@ -8,6 +9,7 @@ import { FireIcon } from "@heroicons/react/solid";
 
 import Link from "v2/components/Link";
 import { injected } from "v2/features/connection/connectors";
+import { networks } from "./networks";
 
 const isRelevantError = (error: Error | undefined): boolean => {
   if (
@@ -26,7 +28,38 @@ const ErrorMessage: FC<{ error: Error | undefined }> = ({ error }) => {
   const { activate } = useWeb3React<Web3Provider>();
 
   if (error instanceof UnsupportedChainIdError) {
-    return <span>{t("v2.connection.unsupportedNetwork")}</span>;
+    return (
+      <>
+        <p>{t("v2.connection.unsupportedNetwork")}</p>
+        <div className="mt-4">
+          {networks.map((network) => (
+            <a
+              key={network.name}
+              href="#"
+              className="inline-flex group justify-between items-center bg-black p-2 rounded-lg transition duration-300 ease-in-out mr-2"
+            >
+              <div className="flex">
+                <div className="w-5 h-5 mr-3">
+                  <Image
+                    src={network.icon}
+                    width={200}
+                    height={200}
+                    layout="responsive"
+                    alt={network.name}
+                    title={network.name}
+                    className="rounded-full"
+                    priority
+                  />
+                </div>
+                <span className="text-white group-hover:text-green-light text-sm font-bold pr-4">
+                  {network.name}
+                </span>
+              </div>
+            </a>
+          ))}
+        </div>
+      </>
+    );
   } else if (
     error instanceof UserRejectedRequestErrorInjected ||
     error instanceof UserRejectedRequestErrorWalletConnect
