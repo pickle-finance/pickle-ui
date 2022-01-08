691adaf25a27f2b803e439048b38fdcdb2d9bb8d
Jan Klimo <jan.klimo@gmail.com>
2022-01-08 19:42:23 +0700

    Connector item with loading state

---
+++ v2/components/Spinner.tsx
@@ -0,0 +1,30 @@
+import { FC, HTMLAttributes } from "react";
+
+import { classNames } from "v2/utils";
+
+interface Props extends HTMLAttributes<HTMLElement> {}
+
+const Spinner: FC<Props> = ({ className }) => (
+  <svg
+    className={classNames("animate-spin text-white", className)}
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <circle
+      className="opacity-25"
+      cx="12"
+      cy="12"
+      r="10"
+      stroke="currentColor"
+      stroke-width="4"
+    />
+    <path
+      className="opacity-75"
+      fill="currentColor"
+      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+    />
+  </svg>
+);
+
+export default Spinner;
