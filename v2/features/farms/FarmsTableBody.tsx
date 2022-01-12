220128c9eecd3a13c09d1dc2074e8edb5215903f
rawbdor <rawblem@gmail.com>
2022-01-12 11:05:22 -0500

    Fixes #276 - only show jars the user is in, but transition could be nicer
    Signed-off-by: rawbdor <rawblem@gmail.com>

--- v2/features/farms/FarmsTableBody.tsx
+++ v2/features/farms/FarmsTableBody.tsx
@@ -4,15 +4,27 @@ import { useSelector } from "react-redux";
 
 import FarmsTableRow from "./FarmsTableRow";
 import { CoreSelectors } from "v2/store/core";
+import { UserSelectors } from "v2/store/user";
+import { UserData, UserTokenData } from "picklefinance-core/lib/client/UserModel";
 
 interface Props {
   simple?: boolean;
+  dashboard?: boolean;
 }
 
-const FarmsTableBody: FC<Props> = ({ simple }) => {
+const FarmsTableBody: FC<Props> = ({ simple, dashboard }) => {
   const { t } = useTranslation("common");
   const loading = useSelector(CoreSelectors.selectLoadingState);
-  const jars = useSelector(CoreSelectors.selectEnabledJars);
+  const userModel: UserData | undefined = useSelector(UserSelectors.selectData);
+
+  // TODO Should be all assets, not just jars
+  let jars = useSelector(CoreSelectors.selectEnabledJars);
+  if( dashboard && userModel) {
+    const empty = (val:string) => val !== undefined && val !== "0";
+    const showAsset = (x:UserTokenData):boolean => !empty(x.pAssetBalance) || !empty(x.pStakedBalance) || !empty(x.picklePending);
+    const apiKeys:string[] = userModel.tokens.filter((x)=> showAsset(x)).map((x)=>x.assetKey);
+    jars = jars.filter((x) => apiKeys.includes(x.details.apiKey));
+  }
 
   if (loading !== "fulfilled") {
     return (
