604425bfdc46880ebc08f316f65ac6d853f84aa0
Jan Klimo <jan.klimo@gmail.com>
2022-01-13 18:41:36 +0700

    User balance status indicator

--- v2/store/user.ts
+++ v2/store/user.ts
@@ -5,10 +5,14 @@ import { RootState } from ".";
 
 interface UserState {
   data: UserData | undefined;
+  isFetching: boolean;
+  updatedAt: number | undefined;
 }
 
 const initialState: UserState = {
   data: undefined,
+  isFetching: false,
+  updatedAt: undefined,
 };
 
 const userSlice = createSlice({
@@ -17,19 +21,27 @@ const userSlice = createSlice({
   reducers: {
     setData: (state, action: PayloadAction<UserData>) => {
       state.data = action.payload;
+      state.updatedAt = +new Date();
+    },
+    setIsFetching: (state, action: PayloadAction<boolean>) => {
+      state.isFetching = action.payload;
     },
   },
 });
 
-export const { setData } = userSlice.actions;
+export const { setData, setIsFetching } = userSlice.actions;
 
 /**
  * Selectors
  */
 const selectData = (state: RootState) => state.user.data;
+const selectIsFetching = (state: RootState) => state.user.isFetching;
+const selectUpdatedAt = (state: RootState) => state.user.updatedAt;
 
 export const UserSelectors = {
   selectData,
+  selectIsFetching,
+  selectUpdatedAt,
 };
 
 export default userSlice.reducer;
