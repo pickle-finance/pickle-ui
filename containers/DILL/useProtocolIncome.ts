c2612befbfef105086b819d7770e29216ad82c66
rawbdor <rawblem@gmail.com>
2022-01-19 23:51:37 -0500

    Fix dill revenue estimates
    Signed-off-by: rawbdor <rawblem@gmail.com>

--- containers/DILL/useProtocolIncome.ts
+++ containers/DILL/useProtocolIncome.ts
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import fetch from "node-fetch";
 import { PoolData } from "../Jars/usePoolData";
+import { PickleCore } from "containers/Jars/usePickleCore";
+import { JarDefinition } from "picklefinance-core/lib/model/PickleModelJson";
 
 type Jar = {
   jarApy: number;
@@ -14,14 +16,22 @@ export function useProtocolIncome() {
     null,
   );
   const { poolData } = PoolData.useContainer();
+  const { pickleCore } = PickleCore.useContainer();
 
   const getWeeklyIncome = async () => {
-    if (poolData) {
-      const profit = poolData.reduce((acc, currJar) => {
-        const jarTVL = currJar.liquidity_locked;
-        return +currJar.jarApy > 0
-          ? acc + (jarTVL * currJar.jarApy * 0.01 * 0.2) / 52
-          : acc;
+    if (pickleCore) {
+      const jars = pickleCore.assets.jars;
+      const profit = jars.reduce((acc, currJar: JarDefinition) => {
+        const jarTVL = currJar.details.harvestStats?.balanceUSD || 0;
+        let apr = currJar.aprStats?.components.filter((x)=>x.compoundable).reduce((acc, curr) => {
+          return acc + curr.apr;
+        }, 0);
+        if( apr === undefined )
+          apr = 0;
+        const chain = pickleCore.chains.find((x)=>x.network === currJar.chain);
+        const perfFee = chain ? chain.defaultPerformanceFee : 0.2;
+        const subtotal = (jarTVL * apr * 0.01 * perfFee) / 52;
+        return acc + subtotal;
       }, 0);
 
       const weeklyDistribution = profit * 0.45;
@@ -33,7 +43,7 @@ export function useProtocolIncome() {
 
   useEffect(() => {
     getWeeklyIncome();
-  }, [poolData]);
+  }, [poolData, pickleCore]);
 
   return {
     weeklyProfit,
