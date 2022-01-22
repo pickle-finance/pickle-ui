be5d1e2256a84ef3ee161a167026be6a5d59f3b5
Jan Klimo <jan.klimo@gmail.com>
2022-01-23 00:18:24 +0700

    Add all tokens

--- v2/features/farms/FarmComponentsIcons.tsx
+++ v2/features/farms/FarmComponentsIcons.tsx
@@ -1,10 +1,56 @@
 import { FC } from "react";
 import Image from "next/image";
+
 import { JarDefinition } from "picklefinance-core/lib/model/PickleModelJson";
+import { classNames } from "v2/utils";
 
 interface Props {
   jar: JarDefinition;
 }
+
+const TokenIcons: FC<Props> = ({ jar }) => {
+  const { components } = jar.depositToken;
+
+  if (!components) return null;
+
+  return (
+    <>
+      {components.map((component, index) => (
+        <div
+          key={component}
+          className={classNames(
+            "image w-10 h-10 rounded-full border-3 border-gray-outline z-10 hover:scale-125 duration-200 hover:z-50",
+            /**
+             * Allow me, dear reader, to ask you not to dismiss this code as something
+             * written by a complete idiot, "clean it up", and ultimately break it.
+             * Tailwind will remove any unused classes in production and unless the classes
+             * are used explicitly in the code, they will get purged. Hence, we can't build
+             * these classes dynamically. We assume no more than 4 components per farm.
+             */
+            index === 0 && "z-40",
+            index === 1 && "z-30",
+            index === 2 && "z-20",
+            index === 3 && "z-10",
+            index > 0 && "-ml-3",
+            index === components.length - 1 && "mr-3",
+          )}
+          // 70% opacity
+          style={{ background: "#2975cab3" }}
+        >
+          <Image
+            src={`/tokens/${component}.png`}
+            className="rounded-full"
+            width={50}
+            height={50}
+            layout="intrinsic"
+            alt={component.toUpperCase()}
+            title={component.toUpperCase()}
+          />
+        </div>
+      ))}
+    </>
+  );
+};
 
 const FarmComponentsIcons: FC<Props> = ({ jar }) => {
   const protocolSrc = `/protocols/${jar.protocol
@@ -13,36 +59,8 @@ const FarmComponentsIcons: FC<Props> = ({ jar }) => {
 
   return (
     <div className="flex relative mr-2">
-      <div
-        className="image w-10 h-10 rounded-full border-3 border-gray-outline z-10 hover:scale-125 duration-200 hover:z-50"
-        // 70% opacity
-        style={{ background: "#2975cab3" }}
-      >
-        <Image
-          src="/usdc.png"
-          className="rounded-full"
-          width={200}
-          height={200}
-          layout="responsive"
-          alt={jar.depositToken.name}
-          title={"USDC"}
-        />
-      </div>
-      <div
-        className="image w-10 h-10 rounded-full border-3 border-gray-outline -ml-3 mr-3 hover:scale-125 duration-200 hover:z-50"
-        style={{ background: "#627eeab3" }}
-      >
-        <Image
-          src="/ethereum.svg"
-          className="rounded-full"
-          width={200}
-          height={200}
-          layout="responsive"
-          alt={jar.depositToken.name}
-          title={"ETH"}
-        />
-      </div>
-      <div className="absolute -top-3 -right-3 w-7 h-7 rounded-full border-3 border-gray-outline -ml-3 mr-3 opacity-0 scale-50 group-hover:scale-100 group-hover:opacity-100 duration-200">
+      <TokenIcons jar={jar} />
+      <div className="absolute -top-3 -right-3 z-50 w-7 h-7 rounded-full border-3 border-gray-outline -ml-3 mr-3 opacity-0 scale-50 group-hover:scale-100 group-hover:opacity-100 duration-200">
         <Image
           src={protocolSrc}
           className="rounded-full"
