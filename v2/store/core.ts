1fdf2260d89cfe2d55b97cc88769c0bac45f400f
Larry the Cucumber <larrythecucumber321@gmail.com>
2022-01-25 20:31:00 -0800

    Add max APY value to dashboard

--- v2/store/core.ts
+++ v2/store/core.ts
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { QueryStatus } from "@reduxjs/toolkit/query";
+import { maxBy } from "lodash";
 import {
   AssetEnablement,
   PickleModelJson,
@@ -50,6 +51,34 @@ const selectEnabledJars = (state: RootState) => {
     (jar) => jar.enablement === AssetEnablement.ENABLED,
   );
 };
+const selectMaxApy = (state: RootState) => {
+  if (state.core.data === undefined) return undefined;
+
+  const jars = state.core.data.assets.jars;
+  const data = jars
+    .filter((jar) => jar.enablement === AssetEnablement.ENABLED)
+    .map((jar) => {
+      let farmApr = 0;
+      const apy = jar.aprStats?.apy || 0;
+      const farmApyComponents = jar.farm?.details?.farmApyComponents;
+
+      if (farmApyComponents && farmApyComponents.length) {
+        farmApr = farmApyComponents[0].apr;
+      }
+
+      return {
+        apiKey: jar.details.apiKey,
+        name: jar.depositToken.name,
+        chain: state.core.data?.chains.find(x=>x.network === jar.chain)?.networkVisible || "Ethereum",
+        apy: apy + farmApr,
+      };
+    })
+    .filter((jar) => jar.apiKey)
+    // When we add a new chain this value will be null and break SSG
+    .filter((jar) => jar.chain);
+
+  return maxBy(data, "apy")!;
+}
 
 const selectLoadingState = (state: RootState) => state.core.loading;
 const selectTimestamp = (state: RootState) => state.core.data?.timestamp;
@@ -59,6 +88,7 @@ export const CoreSelectors = {
   selectEnabledJars,
   selectLoadingState,
   selectTimestamp,
+  selectMaxApy
 };
 
 export default coreSlice.reducer;
