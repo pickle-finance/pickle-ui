31a0f5c2599e5c29f0d0c16020730fa8c3538121
Jan Klimo <jan.klimo@gmail.com>
2022-02-07 17:40:32 +0700

    More cleanup

--- pages/v2/dill.tsx
+++ pages/v2/dill.tsx
@@ -1,38 +1,23 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 import { useTranslation } from "next-i18next";
 import { useSelector } from "react-redux";
 
 import type { PickleFinancePage } from "v2/types";
+import Link from "v2/components/Link";
 import { HistoricChart } from "v2/features/dill/HistoricChart";
 import { RevenueStats } from "v2/features/dill/RevenueStats";
 import { CoreSelectors } from "v2/store/core";
 import { DillInfo } from "v2/features/dill/DillInfo";
 
 const Farms: PickleFinancePage = () => {
-  const [isReadMore, setIsReadMore] = useState(false);
   const core = useSelector(CoreSelectors.selectCore);
-  const { t } = useTranslation("common");
 
   return (
-    <div>
-      <p>
-        {isReadMore
-          ? t("dill.description")
-          : t("dill.description").substring(0, 50)}{" "}
-        <span
-          onClick={() => setIsReadMore(!isReadMore)}
-          className="font-bold cursor-pointer text-orange"
-        >
-          {isReadMore ? "Read less" : "Read more"}
-        </span>
-      </p>
+    <>
       <DillInfo />
-      <h1 className="font-title font-medium text-xl sm:text-xl pt-10 pb-2 pl-1">
-        {t("dill.revenueShareStats")}
-      </h1>
       <RevenueStats dill={core?.dill} />
       <HistoricChart />
-    </div>
+    </>
   );
 };
 
@@ -40,9 +25,22 @@ const PageTitle: FC = () => {
   const { t } = useTranslation("common");
 
   return (
-    <h1 className="font-title font-medium text-2xl sm:text-3xl pt-2">
-      {t("v2.nav.dill")}
-    </h1>
+    <>
+      <h1 className="font-title font-medium text-2xl sm:text-3xl pt-2">
+        {t("v2.nav.dill")}
+      </h1>
+      <h2 className="font-body font-normal text-gray-light text-sm sm:text-base leading-4 sm:leading-6 mt-1">
+        {t("v2.dill.description")}
+        <Link
+          href="https://docs.pickle.finance/dill/vote-locking-pickle-for-dill"
+          external
+          primary
+          className="font-bold ml-1"
+        >
+          {t("v2.dill.readMore")}
+        </Link>
+      </h2>
+    </>
   );
 };
 
