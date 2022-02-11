0f661995c82c16fb4777f1a2493d46e7ef03857f
Jan Klimo <jan.klimo@gmail.com>
2022-02-11 18:56:21 +0700

    Update revenue stats

--- v2/features/dill/RevenueStats.tsx
+++ v2/features/dill/RevenueStats.tsx
@@ -2,7 +2,6 @@ import { FC } from "react";
 import { useTranslation } from "next-i18next";
 
 interface Props {
-  active?: boolean;
   dill?: any;
 }
 
@@ -14,8 +13,9 @@ const RevenueStats: FC<Props> = ({ dill }) => {
         minimumFractionDigits: 0,
         maximumFractionDigits: 0,
       }),
-      title: t("dill.totalLocked"),
+      title: t("v2.dill.totalValueLocked"),
     },
+    { value: "101.78%", title: t("v2.dill.currentAPY") },
     {
       value: Number(
         dill?.dillWeeks[dill.dillWeeks.length - 1].weeklyDillAmount,
@@ -23,10 +23,9 @@ const RevenueStats: FC<Props> = ({ dill }) => {
         minimumFractionDigits: 0,
         maximumFractionDigits: 0,
       }),
-      title: t("dill.weeklyReward"),
+      title: t("v2.dill.upcomingDistributionValue"),
     },
-    { value: "101.78%", title: t("dill.currentAPY") },
-    { value: "Thu, Jun 10 2021", title: t("dill.nextDistribution") },
+    { value: "Thu, Jun 10 2021", title: t("v2.dill.upcomingDistributionDate") },
   ];
 
   return (
@@ -35,11 +34,15 @@ const RevenueStats: FC<Props> = ({ dill }) => {
         {t("v2.dill.revenueShareStats")}
       </h1>
       <div className="flex justify-between align-center bg-black-light rounded-xl border border-gray-dark shadow px-6 py-7">
-        {items?.map((item, i) => (
-          <aside key={i}>
-            <p>{item.value}</p>
-            <p>{item.title}</p>
-          </aside>
+        {items.map(({ value, title }) => (
+          <div key={title}>
+            <h2 className="font-title font-medium text-white text-lg leading-5">
+              {value}
+            </h2>
+            <p className="font-body text-gray-light font-normal text-xs leading-4">
+              {title}
+            </p>
+          </div>
         ))}
       </div>
     </>
