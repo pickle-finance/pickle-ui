25032513d8cc6403698f9ccfe107cf40a7531a6e
Jan Klimo <jan.klimo@gmail.com>
2022-03-16 19:45:17 +0700

    Use user model callbacks
    Fetch user model and core data in intervals

--- v2/providers/Web3Provider.tsx
+++ v2/providers/Web3Provider.tsx
@@ -2,14 +2,8 @@ import { FC, useEffect } from "react";
 import { Web3ReactProvider, useWeb3React } from "@web3-react/core";
 import { useSelector } from "react-redux";
 import { Web3Provider } from "@ethersproject/providers";
-import { useRouter } from "next/router";
 
-import { useAppDispatch } from "v2/store";
-import { updateBlockNumber } from "v2/store/connection";
-import {
-  useEagerConnect,
-  useInactiveListener,
-} from "v2/features/connection/hooks";
+import { useEagerConnect, useInactiveListener } from "v2/features/connection/hooks";
 import { injected } from "v2/features/connection/connectors";
 import { ConnectionSelectors } from "v2/store/connection";
 
@@ -17,11 +11,7 @@ const getLibrary = (provider: any) => new Web3Provider(provider);
 
 const AppWeb3Provider: FC = ({ children }) => {
   const { active, error, activate, library } = useWeb3React<Web3Provider>();
-  const isManuallyDeactivated = useSelector(
-    ConnectionSelectors.selectIsManuallyDeactivated,
-  );
-  const router = useRouter();
-  const dispatch = useAppDispatch();
+  const isManuallyDeactivated = useSelector(ConnectionSelectors.selectIsManuallyDeactivated);
 
   // Try to eagerly connect to an injected provider, if it exists and has granted access already
   const triedEager = useEagerConnect();
@@ -36,23 +26,6 @@ const AppWeb3Provider: FC = ({ children }) => {
 
   // When there's no account connected, react to logins (broadly speaking) on the injected provider, if it exists
   useInactiveListener(!triedEager);
-
-  useEffect(() => {
-    if (library) {
-      const { ethereum } = window;
-
-      library.on("block", (blockNumber: number) =>
-        dispatch(updateBlockNumber(blockNumber)),
-      );
-
-      return () => {
-        if (ethereum?.removeListener) {
-          ethereum.removeListener("chainChanged", router.reload);
-        }
-        library.removeAllListeners("block");
-      };
-    }
-  }, [library]);
 
   return <>{children}</>;
 };
--- v2/store/user.ts
+++ v2/store/user.ts
@@ -1,4 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { cloneDeep } from "lodash";
 import { UserData } from "picklefinance-core/lib/client/UserModel";
 
 import { RootState } from ".";
@@ -15,13 +16,30 @@ const initialState: UserState = {
   updatedAt: undefined,
 };
 
+type UpdateType = "incremental" | "final";
+
 const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    setData: (state, action: PayloadAction<UserData>) => {
-      state.data = action.payload;
-      state.updatedAt = +new Date();
+    setData: (state, action: PayloadAction<{ data: UserData; type: UpdateType }>) => {
+      /**
+       * A deep clone is important because if we dispatch data
+       * passed to user model callbacks, Redux will make it
+       * read-only, thus breaking the internals of UserModel.
+       *
+       * Update state incrementally on initial page load. Only
+       * update with the final user model on consequent runs to
+       * avoid UI from going from populated to empty and back.
+       */
+      if (action.payload.type === "incremental") {
+        if (state.updatedAt) return;
+
+        state.data = cloneDeep(action.payload.data);
+      } else if (action.payload.type === "final") {
+        state.data = cloneDeep(action.payload.data);
+        state.updatedAt = +new Date();
+      }
     },
     setIsFetching: (state, action: PayloadAction<boolean>) => {
       state.isFetching = action.payload;
