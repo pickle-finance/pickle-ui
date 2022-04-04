4451d24fefccc378dc3833ec41da3dc21f7179c7
bcstryker <bcstryker@gmail.com>
2022-04-04 17:12:42 -0400

    reorganized codebase with intention to have single voting page

--- v2/features/dill/vote/VoteButtonSideChain.tsx
+++ v2/features/dill/vote/VoteButtonSideChain.tsx
@@ -2,33 +2,31 @@ import { Web3Provider } from "@ethersproject/providers";
 import { useTranslation } from "next-i18next";
 import { FC } from "react";
 
+
 const VoteButton: FC<{
-  vote: voteFunction;
-  provider: Web3Provider | undefined;
-  account: string | null | undefined;
-  selectedChains: string[];
-  selectedJars: string[];
-  selectedStrats: string[];
-}> = ({ vote, provider, account, selectedChains, selectedJars, selectedStrats }) => {
+  vote: voteFunction, 
+  provider: Web3Provider | undefined,
+  account: string | null | undefined,
+  selectedChains: string[],
+  selectedJars: string[]
+}> = ({vote, provider, account, selectedChains, selectedJars}) => {
   const { t } = useTranslation("common");
   return (
-    <div className="pb-10 mb-5 mt-10">
-      <button
-        className="float-right rounded p-2 border border-foreground-alt-400 bg-background text-foreground-alt-200"
-        onClick={() => vote(provider, account, selectedChains, selectedJars, selectedStrats)}
+    <div className="pb-10">
+      <button className="float-right rounded p-2 border border-foreground-alt-400 bg-background text-foreground-alt-200"
+        onClick={() => vote(provider, account, selectedChains, selectedJars)}
       >
         {t("v2.dill.vote.castOffchainVote")}
       </button>
     </div>
-  );
-};
+  )
+}
 
 export type voteFunction = (
   provider: Web3Provider | undefined,
   account: string | null | undefined,
   selectedChain: string[],
   selectedJars: string[],
-  selectedStrats: string[]
 ) => void;
 
 export default VoteButton;
