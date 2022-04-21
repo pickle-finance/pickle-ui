244b9572e54f101bb4927fc1a451c99b6b1c4d85
Vivek Jain <26vivek06@gmail.com>
2022-04-21 22:57:12 +0530

    feat: add swap page

---
+++ pages/v2/swap.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react";
+import { useTranslation } from "next-i18next";
+import SwapMainCard from "v2/features/swap/SwapMainCard";
+
+const Swap = () => {
+  const { t } = useTranslation("common");
+
+  return <SwapMainCard />;
+};
+
+const PageTitle: FC = () => {
+  const { t } = useTranslation("common");
+
+  return <h1 className="font-title font-medium text-2xl sm:text-3xl pt-2">{t("v2.nav.swap")}</h1>;
+};
+
+Swap.PageTitle = PageTitle;
+Swap.clearScreen = true;
+
+export { getStaticProps } from "../../util/locales";
+
+export default Swap;
