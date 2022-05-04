4293d5f3e973a55c8732f3442e651869cd92c7c7
Larry the Cucumber <larrythecucumber321@gmail.com>
2022-05-04 13:22:07 -0700

    v2: fix display of jar controls without farms

--- v2/features/farms/FarmTableRowBodyTransactionControls.tsx
+++ v2/features/farms/FarmTableRowBodyTransactionControls.tsx
@@ -1,6 +1,8 @@
 import { FC } from "react";
 import { useTranslation } from "next-i18next";
 import { BigNumber, ethers } from "ethers";
+import { useWeb3React } from "@web3-react/core";
+import { Web3Provider } from "@ethersproject/providers";
 
 import { useAppSelector } from "v2/store";
 import { JarWithData } from "v2/store/core";
@@ -13,9 +15,8 @@ import DepositFlow from "./flows/deposit/DepositFlow";
 import WithdrawFlow from "./flows/withdraw/WithdrawFlow";
 import StakeFlow from "./flows/stake/StakeFlow";
 import UnstakeFlow from "./flows/unstake/UnstakeFlow";
-import { roundToSignificantDigits } from "v2/utils";
+import { classNames, roundToSignificantDigits } from "v2/utils";
 import HarvestFlow from "./flows/harvest/HarvestFlow";
-import { useAccount } from "v2/hooks";
 
 interface Props {
   jar: JarWithData;
@@ -23,7 +24,7 @@ interface Props {
 
 const FarmsTableRowBodyTransactionControls: FC<Props> = ({ jar }) => {
   const { t } = useTranslation("common");
-  const account = useAccount();
+  const { account } = useWeb3React<Web3Provider>();
 
   const isUserModelLoading = useAppSelector(UserSelectors.selectIsFetching);
   const userTokenData = useAppSelector((state) =>
@@ -45,53 +46,28 @@ const FarmsTableRowBodyTransactionControls: FC<Props> = ({ jar }) => {
 
   return (
     <div className="flex space-x-3">
-      <div className="grow border self-start border-foreground-alt-500 rounded-xl p-4">
-        <p className="font-title text-foreground-alt-200 font-medium text-base leading-5 mb-2">
-          {t("v2.farms.depositedToken", { token: jar.depositToken.name })}
-        </p>
-        <div className="flex items-end justify-between">
-          <span className="font-title text-primary font-medium text-base leading-5">
-            {jarTokens}
-          </span>
-          <ApprovalFlow
-            apiKey={jar.details.apiKey}
-            tokenAddress={jar.depositToken.addr}
-            tokenName={jar.depositToken.name}
-            spenderAddress={jar.contract}
-            storeAttribute="jarAllowance"
-            chainName={jar.chain}
-            visible={!userHasJarAllowance}
-          />
-          {userHasJarAllowance && (
-            <div className="grid grid-cols-2 gap-3">
-              <DepositFlow jar={jar} balances={userTokenData} />
-              <WithdrawFlow jar={jar} balances={userTokenData} />
-            </div>
-          )}
-        </div>
-      </div>
-      <div className="grow self-start">
+      <div className={classNames(jarSupportsStaking(jar) ? "grow self-start" : "w-1/2")}>
         <div className="border border-foreground-alt-500 rounded-xl p-4">
           <p className="font-title text-foreground-alt-200 font-medium text-base leading-5 mb-2">
-            {t("v2.farms.stakedToken", { token: jar.depositToken.name })}
+            {t("v2.farms.depositedToken", { token: jar.depositToken.name })}
           </p>
           <div className="flex items-end justify-between">
             <span className="font-title text-primary font-medium text-base leading-5">
-              {farmTokens}
+              {jarTokens}
             </span>
             <ApprovalFlow
               apiKey={jar.details.apiKey}
-              tokenAddress={jar.contract}
-              tokenName={jar.farm?.farmDepositTokenName}
-              spenderAddress={jar.farm?.farmAddress}
-              storeAttribute="farmAllowance"
+              tokenAddress={jar.depositToken.addr}
+              tokenName={jar.depositToken.name}
+              spenderAddress={jar.contract}
+              storeAttribute="jarAllowance"
               chainName={jar.chain}
-              visible={!userHasFarmAllowance}
+              visible={!userHasJarAllowance}
             />
-            {userHasFarmAllowance && (
+            {userHasJarAllowance && (
               <div className="grid grid-cols-2 gap-3">
-                <StakeFlow jar={jar} balances={userTokenData} />
-                <UnstakeFlow jar={jar} balances={userTokenData} />
+                <DepositFlow jar={jar} balances={userTokenData} />
+                <WithdrawFlow jar={jar} balances={userTokenData} />
               </div>
             )}
           </div>
@@ -102,30 +78,60 @@ const FarmsTableRowBodyTransactionControls: FC<Props> = ({ jar }) => {
           )}
         </div>
       </div>
+      {/* Stake and Harvest functions hidden if no staking available */}
       {jarSupportsStaking(jar) && (
-        <div className="grow self-start">
-          <div className="border border-foreground-alt-500 rounded-xl p-4">
-            <p className="font-title text-foreground-alt-200 font-medium text-base leading-5 mb-2">
-              {t("v2.farms.earnedToken", { token: "PICKLEs" })}
-            </p>
-            <div className="flex items-end justify-between">
-              <span className="font-title text-primary font-medium text-base leading-5">
-                {roundToSignificantDigits(picklePending, 3)}
-              </span>
-              <HarvestFlow
-                rewarderType="farm"
-                asset={jar}
-                harvestableAmount={BigNumber.from(userTokenData?.picklePending || 0)}
-                network={jar.chain}
-              />
+        <>
+          <div className="grow self-start">
+            <div className="border border-foreground-alt-500 rounded-xl p-4">
+              <p className="font-title text-foreground-alt-200 font-medium text-base leading-5 mb-2">
+                {t("v2.farms.stakedToken", { token: jar.depositToken.name })}
+              </p>
+              <div className="flex items-end justify-between">
+                <span className="font-title text-primary font-medium text-base leading-5">
+                  {farmTokens}
+                </span>
+                <ApprovalFlow
+                  apiKey={jar.details.apiKey}
+                  tokenAddress={jar.contract}
+                  tokenName={jar.farm?.farmDepositTokenName}
+                  spenderAddress={jar.farm?.farmAddress}
+                  storeAttribute="farmAllowance"
+                  chainName={jar.chain}
+                  visible={!userHasFarmAllowance}
+                />
+                {userHasFarmAllowance && (
+                  <div className="grid grid-cols-2 gap-3">
+                    <StakeFlow jar={jar} balances={userTokenData} />
+                    <UnstakeFlow jar={jar} balances={userTokenData} />
+                  </div>
+                )}
+              </div>
             </div>
           </div>
-          <div className="relative">
-            {isUserModelLoading && (
-              <LoadingIndicator waitForUserModel className="absolute r-0 t-0 mt-1" />
-            )}
+          <div className="grow self-start">
+            <div className="border border-foreground-alt-500 rounded-xl p-4">
+              <p className="font-title text-foreground-alt-200 font-medium text-base leading-5 mb-2">
+                {t("v2.farms.earnedToken", { token: "PICKLEs" })}
+              </p>
+              <div className="flex items-end justify-between">
+                <span className="font-title text-primary font-medium text-base leading-5">
+                  {roundToSignificantDigits(picklePending, 3)}
+                </span>
+                <HarvestFlow
+                  rewarderType="farm"
+                  asset={jar}
+                  harvestableAmount={BigNumber.from(userTokenData?.picklePending || 0)}
+                  network={jar.chain}
+                />
+              </div>
+            </div>
+            <div className="relative">
+              {isUserModelLoading && (
+                <LoadingIndicator waitForUserModel className="absolute r-0 t-0 mt-1" />
+              )}
+            </div>
           </div>
-        </div>
+        </>
       )}
     </div>
   );
