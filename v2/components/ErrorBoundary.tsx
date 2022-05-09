b6cd11cadb894d16f43b84912faf840a977f875b
bcstryker <bcstryker@gmail.com>
2022-05-09 19:26:54 -0400

    error boundaries added, should come up with a more impressive failure modal

--- v2/components/ErrorBoundary.tsx
+++ v2/components/ErrorBoundary.tsx
@@ -1,4 +1,7 @@
-import React, { Component, ErrorInfo, ReactNode } from "react";
+import { useTranslation } from "next-i18next";
+import React, { Component, ErrorInfo, FC, ReactNode } from "react";
+import { classNames } from "v2/utils";
+import Link from "v2/components/Link";
 
 interface Props {
   children: ReactNode;
@@ -25,23 +28,33 @@ class ErrorBoundary extends Component<Props, State> {
   public render() {
     const { hasError } = this.state;
     if (hasError) {
-      return (
-        <div
-          style={{
-            color: "red",
-            border: "1px solid red",
-            borderRadius: "0.25rem",
-            margin: "0.5rem",
-            padding: "0.5rem",
-          }}
-        >
-          An error was thrown.
-        </div>
-      );
+      return <ErrorMessage />;
     }
-
     return this.props.children;
   }
 }
+
+const ErrorMessage: FC<{ className?: string }> = ({ className }) => {
+  const { t } = useTranslation("common");
+  return (
+    <div className="flex flex-col justify-center items-center py-96">
+      <div
+        className={classNames(
+          "bg-background-light w-1/3 indent-4 rounded-xl border border-foreground-alt-500 shadow p-12",
+          className,
+        )}
+      >
+        <div className="w-full text-justify">
+          <p className="break-normal text-accent">{t("v2.error")}</p>
+        </div>
+        <div className="text-center">
+          <Link className="mt-10" href="https://discord.gg/XpJZ8vwS" external>
+            <p className="on-hover:text-primary-light">https://discord.gg/XpJZ8vwS</p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ErrorBoundary;
