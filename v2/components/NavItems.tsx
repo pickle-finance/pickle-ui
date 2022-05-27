34857111735d5af02b7a5a39e471cf2aeadf1706
Jan Klimo <jan.klimo@gmail.com>
2022-05-27 10:03:38 +0700

    Finalize nav items

--- v2/components/NavItems.tsx
+++ v2/components/NavItems.tsx
@@ -1,13 +1,13 @@
 import { FC, MouseEventHandler } from "react";
 import {
+  AcademicCapIcon,
   ChartBarIcon,
+  HeartIcon,
   LightningBoltIcon,
-  QuestionMarkCircleIcon,
   SpeakerphoneIcon,
+  StarIcon,
   TemplateIcon,
   UserGroupIcon,
-  StarIcon,
-  HeartIcon,
 } from "@heroicons/react/solid";
 import { useTranslation } from "next-i18next";
 
@@ -31,14 +31,30 @@ const NavItems: FC<Props> = ({ onClick }) => {
     { name: t("v2.nav.vote"), href: "/v2/vote", icon: StarIcon },
     { name: t("v2.nav.brinery"), href: "/v2/brinery", icon: HeartIcon },
     { name: t("v2.nav.stats"), href: "/v2/stats", icon: ChartBarIcon },
-    { name: t("v2.nav.faq"), href: "#", icon: QuestionMarkCircleIcon },
-    { name: t("v2.nav.feedback"), href: "#", icon: SpeakerphoneIcon },
+    {
+      name: t("v2.nav.docs"),
+      href: "https://docs.pickle.finance/",
+      icon: AcademicCapIcon,
+      external: true,
+    },
+    {
+      name: t("v2.nav.feedback"),
+      href: "https://forum.pickle.finance/",
+      icon: SpeakerphoneIcon,
+      external: true,
+    },
   ];
 
   return (
     <nav className="flex-1 space-y-2">
       {navigation.map((item) => (
-        <NavItem key={item.name} Icon={item.icon} href={item.href} onClick={onClick}>
+        <NavItem
+          key={item.name}
+          Icon={item.icon}
+          href={item.href}
+          external={item.external}
+          onClick={onClick}
+        >
           {item.name}
         </NavItem>
       ))}
