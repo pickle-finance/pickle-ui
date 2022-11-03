168c4c5114ee8554cafbb73ac6bccb2055b69419
Larry the Cucumber <larrythecucumber321@gmail.com>
2022-11-03 12:11:07 -0700

    chore: temp remove injected provider

--- v2/providers/Web3Provider.tsx
+++ v2/providers/Web3Provider.tsx
@@ -6,31 +6,29 @@ import { useEagerConnect, useInactiveListener } from "v2/features/connection/hoo
 import { connectorsAndHooks, injected } from "v2/features/connection/connectors";
 import { ConnectionSelectors } from "v2/store/connection";
 
-const AppWeb3Provider: FC<PropsWithChildren> = ({ children }) => {
-  const { active, error, activate, library } = useWeb3React<Web3Provider>();
-  const isManuallyDeactivated = useSelector(ConnectionSelectors.selectIsManuallyDeactivated);
+// const AppWeb3Provider: FC<PropsWithChildren> = ({ children }) => {
+//   const { active, error, activate, library } = useWeb3React<Web3Provider>();
+//   const isManuallyDeactivated = useSelector(ConnectionSelectors.selectIsManuallyDeactivated);
 
-  // Try to eagerly connect to an injected provider, if it exists and has granted access already
-  const triedEager = useEagerConnect();
+//   // Try to eagerly connect to an injected provider, if it exists and has granted access already
+//   const triedEager = useEagerConnect();
 
-  // After eagerly trying injected, if the network connect ever isn't active or in an error state, activate it
-  // but only if the user hasn't manually deactivated the connection (clicked Exit)
-  useEffect(() => {
-    if (triedEager && !error && !active && !isManuallyDeactivated) {
-      activate(injected);
-    }
-  }, [triedEager, error, active]);
+//   // After eagerly trying injected, if the network connect ever isn't active or in an error state, activate it
+//   // but only if the user hasn't manually deactivated the connection (clicked Exit)
+//   useEffect(() => {
+//     if (triedEager && !error && !active && !isManuallyDeactivated) {
+//       activate(injected);
+//     }
+//   }, [triedEager, error, active]);
 
-  // When there's no account connected, react to logins (broadly speaking) on the injected provider, if it exists
-  useInactiveListener(!triedEager);
+//   // When there's no account connected, react to logins (broadly speaking) on the injected provider, if it exists
+//   useInactiveListener(!triedEager);
 
-  return <>{children}</>;
-};
+//   return <>{children}</>;
+// };
 
 const Provider: FC<PropsWithChildren> = ({ children }) => (
-  <Web3ReactProvider connectors={connectorsAndHooks}>
-    <AppWeb3Provider>{children}</AppWeb3Provider>
-  </Web3ReactProvider>
+  <Web3ReactProvider connectors={connectorsAndHooks}>{children}</Web3ReactProvider>
 );
 
 export default Provider;
