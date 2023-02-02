6a487482dce54c2e07a7d0667d03a073d36ff968
rawbdor <rawblem@gmail.com>
2023-02-02 10:23:45 -0500

    Fix some bugs in user page
    Signed-off-by: rawbdor <rawblem@gmail.com>

--- v2/features/stats/UserStats.tsx
+++ v2/features/stats/UserStats.tsx
@@ -1,3 +1,4 @@
+import { classNames } from "v2/utils";
 import { FC, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { PickleModelJson } from "picklefinance-core";
@@ -15,7 +16,7 @@ import {
 import { useAccount } from "v2/hooks";
 import { readyState } from "pages/stats";
 import { SetFunction } from "v2/types";
-import { UserStatsReady } from "pages/robtest";
+import { UserStatsReady } from "pages/userHistory";
 
 export interface UserAssetsHistoryWrapper {
   [apiKey: string]: PnlTransactionWrapper[];
@@ -33,7 +34,8 @@ const UserStats: FC<{
 
   const [userHistory, setUserHistory] = useState<UserJarHistory>();
   const [wrappedUserHistory, setWrappedUserHistory] = useState<UserAssetsHistoryWrapper>();
-
+  const [failedUserHistory, setFailedUserHistory] = useState<string[]>();
+  
 
   // fetch all user txn history if there is any
   useEffect(() => {
@@ -44,6 +46,7 @@ const UserStats: FC<{
           .then((jsonResp) => {
             if (!JSON.stringify(jsonResp).toLowerCase().includes("invalid wallet")) {
               setUserHistory(jsonResp);
+              const failed: string[] = [];
               const keys = Object.keys(jsonResp);
               const allAssetsHistory: UserAssetsHistoryWrapper = {};
               for( let i = 0; i < keys.length; i++ ) {
@@ -52,10 +55,11 @@ const UserStats: FC<{
                   let pnl = new UserJarHistoryPnlGenerator(account, v).generatePnL();
                   allAssetsHistory[keys[i]] = pnl;
                 } catch( e ) {
-                  // ignore
+                  failed.push(keys[i]);
                 }
               }
               setWrappedUserHistory(allAssetsHistory);
+              setFailedUserHistory(failed);
             } else {
               console.info(jsonResp);
             }
@@ -69,9 +73,11 @@ const UserStats: FC<{
   if (core) {
     // generate pnl report for one jar from user history if/when it loads
     // let pnl = new UserJarHistoryPnlGenerator(account, userHistory[jar.value]).generatePnL();
+    const safeFailed: string[] = failedUserHistory || [];
     const t1: UserAssetsHistoryWrapper | undefined = wrappedUserHistory;
     const t2: UserAssetsHistoryWrapper = t1 ? t1 : {};
     const keys = Object.keys(t2);
+    const allKeys = safeFailed.concat(keys);
     const findJar = (apiKey: string): JarDefinition | undefined => core.assets.jars.find((j) => j && j.details && j.details.apiKey && j.details.apiKey.toLowerCase() === apiKey.toLowerCase());
     const addrsFor = (apiKey: string) => {
       const j1: JarDefinition | undefined = findJar(apiKey);
@@ -79,7 +85,7 @@ const UserStats: FC<{
         Object.entries({
           User: account ? account.toLowerCase() : "account not found",
           Jar: j1 ? j1.contract.toLowerCase() : "jar not found",
-          Farm: j1 && j1.farm ? j1.farm.farmAddress.toLowerCase() : "farm not found",
+          Farm: j1 && j1.farm && j1.farm.farmAddress ? j1.farm.farmAddress.toLowerCase() : "farm not found",
           Null: "0x0000000000000000000000000000000000000000",
         }).map(([key, value]) => [value, key])
       );
@@ -91,16 +97,32 @@ const UserStats: FC<{
     };
     return (
       <>
-        {keys.map((k:string) => 
-          (
-            <TxHistoryContainer wallet={account || ""} userPnl={t2[k]} core={core} addrs={addrsFor(k)} jar={findJar(k) || {} as JarDefinition} />
-          )
+        {allKeys.map((k:string) => 
+          safeFailed.includes(k) ? 
+          (<UserStatFailedComponent core={core} apiKey={k} />)
+          : (<TxHistoryContainer wallet={account || ""} userPnl={t2[k]} core={core} addrs={addrsFor(k)} jar={findJar(k) || {} as JarDefinition} />)
         )}
       </>
     );
   }
   return null;
 };
+
+const UserStatFailedComponent: FC<{
+  core: PickleModelJson.PickleModelJson | undefined;
+  apiKey: string;
+}> = ({ core, apiKey}) => {
+  const label = "Failed to load user history for asset " + apiKey;
+  return (
+    <div className={classNames("pr-5 mb-8 min-w-min")}>
+      <div className="flex">
+        <h2 className="whitespace-nowrap font-body font-bold text-xl text-foreground-alt-200 mt-3 mb-8">
+          {label}
+        </h2>
+      </div>
+    </div>
+  );
+}
 
 
 const DEBUG_OUT = (msg: any) => {
